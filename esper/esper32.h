d93635f922c47f3b9048c93e40faa48d321cce62
hiroto <nikoharianto58@gmail.com>
2022-07-10 01:03:48 +0700

    Update esper32.h

--- esper/esper32.h
+++ esper/esper32.h
@@ -10,7 +10,7 @@ typedef union _ULARGE_INTEGER32 {
     uint16_t high;
   };
   uint32_t dword_part;
-}ULARGE_INTEGER32, *LPULARGE_INTEGER32;
+}ULARGE_INTEGER32;
 
 static uint32_t esp32state = 0;
  
@@ -27,5 +27,5 @@ uint32_t esper32(void) {
   lpdata.low ^= (lpdata.low | (((lpdata.high) >> 8) & 0xff));
   lpdata.high ^= (lpdata.low | (lpdata.low >> 8));
   
-  return (uint32_t)(esp32state += ((esp32state += 0x7ffff) ^ (lpdata.dword_part >> lpdata.high)));
+  return (uint32_t)(esp32state += ((esp32state += 0x7ffff) ^ (lpdata.dword_part << lpdata.high)));
 }
