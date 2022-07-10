acddffdd64b74f44eaf90d8abb669dbb760addb5
hiroto <nikoharianto58@gmail.com>
2022-07-10 17:03:54 +0700

    Update esper32.h

--- esper/esper32.h
+++ esper/esper32.h
@@ -27,5 +27,5 @@ uint32_t esper32(void) {
   lpdata.low ^= (lpdata.low | (lpdata.high >> 8));
   lpdata.high ^= (lpdata.low | (lpdata.dword_part >> 8));
   
-  return (uint32_t)(esp32state += ((esp32state += 0x7ffff) ^ (lpdata.dword_part << lpdata.high)));
+  return (uint32_t)(esp32state += ((esp32state += 0x7ffff) ^ (lpdata.low & lpdata.high)));
 }
